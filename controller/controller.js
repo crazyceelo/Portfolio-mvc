b4ddb59bf5a5e9f65119a24cea2cc7757c4691c7
Carlos Perez <solracias@gmail.com>
2017-07-20 18:13:20 -0700

    added main handlebars code

--- controller/controller.js
+++ controller/controller.js
@@ -3,7 +3,13 @@
 var express = require("express");
 var router = express.Router();
 
-// Create all our routes and set up logic within those routes where required.
+// import any models here
+    // var db = require("../models");
 
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(request, response){
+    response.render("index");
+})
 
 module.exports = router;
